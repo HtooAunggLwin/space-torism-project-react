27fae4f706d43f537879e45cac8acd04ba167b41
test <test@test.com>
2022-05-21 12:41:28 +0630

    Project starter files

---
+++ src/components/DesignSystem.jsx
@@ -0,0 +1,13 @@
+import './DesignSystem.css';
+
+const DesignSystem = () => {
+    return ( 
+        <div className="design-system">
+            <div className="test bg-white">
+                <h1>Hello world!</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default DesignSystem;
